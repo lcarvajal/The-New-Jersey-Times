061cf381cf6333531eade27342e5ba0db6c1b3fd
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-25 01:35:08 +0200

    Style buttons blue with rounded corners

--- src/components/NewspaperHeader/ChangeLanguageButtons.js
+++ src/components/NewspaperHeader/ChangeLanguageButtons.js
@@ -4,6 +4,7 @@ export default function ChangeLanguageButtons() {
     { id: 1, title: 'International' },
     { id: 2, title: 'Canada' },
     { id: 3, title: 'Español' },
+    { id: 4, title: '中文' },
   ]
   const listItems = links.map(link => <li><a key={link.id} href="">{link.title}</a></li>)
 
