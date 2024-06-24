6438b31493933c57ced1de0fc378b53009b639bd
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-25 01:57:34 +0200

    Add static stock picker

--- src/components/NewspaperHeader/StockTicker.js
+++ src/components/NewspaperHeader/StockTicker.js
@@ -1,7 +1,8 @@
 export default function StockTicker() {
   return (
     <div className="StockTicker">
-      <p>Nasdaq -></p>
+      <p>Nasdaq +0.03% <svg aria-hidden="true" width="5px" height="9px" viewBox="0 0 5 9" class=""><title>Group</title><defs></defs><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="Artboard"><g id="Group"><path d="M2.5,9 L2.5,3.373" id="Shape" stroke="#3f893b"></path><polyline id="Shape" fill="#3f893b" fill-rule="nonzero" points="4.86293961 4.09179688 2.5 0 0.13706039 4.09179688"></polyline></g></g></g></svg>
+      </p>
     </div>
   )
 }
