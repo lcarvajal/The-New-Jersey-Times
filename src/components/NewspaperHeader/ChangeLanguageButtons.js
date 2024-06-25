6e6522b07a306cffb33e5d9302e5e560ea1086dd
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-25 03:21:57 +0200

    All caps language button titles

--- src/components/NewspaperHeader/ChangeLanguageButtons.js
+++ src/components/NewspaperHeader/ChangeLanguageButtons.js
@@ -1,9 +1,9 @@
 export default function ChangeLanguageButtons() {
   const links = [
     { id: 0, title: 'U.S.' },
-    { id: 1, title: 'International' },
-    { id: 2, title: 'Canada' },
-    { id: 3, title: 'Español' },
+    { id: 1, title: 'INTERNATIONAL' },
+    { id: 2, title: 'CANADA' },
+    { id: 3, title: 'ESPAÑOL' },
     { id: 4, title: '中文' },
   ]
   const listItems = links.map(link => <li><a key={link.id} href="">{link.title}</a></li>)
