1dd285a5cd6b58ecd1c340b414d782ab910f746e
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-25 11:39:47 +0200

    Add key to li elements

--- src/components/NewspaperHeader/NavigationButtons.js
+++ src/components/NewspaperHeader/NavigationButtons.js
@@ -7,14 +7,14 @@ export default function NavigationButtons() {
     { id: 4, title: 'Lifestyle' },
   ]
   const rightLinks = [
-    { id: 0, title: 'Audio' },
-    { id: 1, title: 'Games' },
-    { id: 2, title: 'Cooking' },
-    { id: 3, title: 'Wirecutter' },
-    { id: 4, title: 'The Athletic' },
+    { id: 100, title: 'Audio' },
+    { id: 101, title: 'Games' },
+    { id: 102, title: 'Cooking' },
+    { id: 103, title: 'Wirecutter' },
+    { id: 104, title: 'The Athletic' },
   ]
-  const leftListItems = leftLinks.map(link => <li><a key={link.id} href="">{link.title} <svg class="css-1gq8co7" viewBox="0 0 13 8"><polygon fill="#979797" points="6.5,8 0,1.4 1.4,0 6.5,5.2 11.6,0 13,1.4"></polygon></svg></a></li>)
-  const rightListItems = rightLinks.map(link => <li><a key={link.id} href="">{link.title} <svg class="css-1gq8co7" viewBox="0 0 13 8"><polygon fill="#979797" points="6.5,8 0,1.4 1.4,0 6.5,5.2 11.6,0 13,1.4"></polygon></svg></a></li>)
+  const leftListItems = leftLinks.map(link => <li key={link.id}><a key={link.id} href="">{link.title} <svg class="css-1gq8co7" viewBox="0 0 13 8"><polygon fill="#979797" points="6.5,8 0,1.4 1.4,0 6.5,5.2 11.6,0 13,1.4"></polygon></svg></a></li>)
+  const rightListItems = rightLinks.map(link => <li key={link.id}><a key={link.id} href="">{link.title} <svg class="css-1gq8co7" viewBox="0 0 13 8"><polygon fill="#979797" points="6.5,8 0,1.4 1.4,0 6.5,5.2 11.6,0 13,1.4"></polygon></svg></a></li>)
 
   return (
     <div className="NavigationButtons">
