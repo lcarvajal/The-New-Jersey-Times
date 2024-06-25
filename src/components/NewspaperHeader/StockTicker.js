5b000653ed87288266655cb6d3b8c565c9cef5ad
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-25 11:48:21 +0200

    Update svg attributes to React names instead of html names

--- src/components/NewspaperHeader/StockTicker.js
+++ src/components/NewspaperHeader/StockTicker.js
@@ -34,9 +34,9 @@ export default function StockTicker() {
     <div className="StockTicker">
       <p transition="0.8s linear">{stockTicker.name}
         {stockTicker.percentageChange < 0 ? (
-          <span className="TextRed">{` ${stockTicker.percentageChange}%`} <svg aria-hidden="true" width="5px" height="9px" viewBox="0 0 5 9" className="StockTickerDownArrow"><title>Group</title><defs></defs><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="Artboard"><g id="Group"><path d="M2.5,9 L2.5,3.373" id="Shape" stroke="#a61b1e"></path><polyline id="Shape" fill="#a61b1e" fill-rule="nonzero" points="4.86293961 4.09179688 2.5 0 0.13706039 4.09179688"></polyline></g></g></g></svg></span>
+          <span className="TextRed">{` ${stockTicker.percentageChange}%`} <svg aria-hidden="true" width="5px" height="9px" viewBox="0 0 5 9" className="StockTickerDownArrow"><title>Group</title><defs></defs><g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd"><g id="Artboard"><g id="Group"><path d="M2.5,9 L2.5,3.373" id="Shape" stroke="#a61b1e"></path><polyline id="Shape" fill="#a61b1e" fillRule="nonzero" points="4.86293961 4.09179688 2.5 0 0.13706039 4.09179688"></polyline></g></g></g></svg></span>
         ) : (
-          <span className="TextGreen">{` +${stockTicker.percentageChange}%`} <svg aria-hidden="true" width="5px" height="9px" viewBox="0 0 5 9" class=""><title>Group</title><defs></defs><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="Artboard"><g id="Group"><path d="M2.5,9 L2.5,3.373" id="Shape" stroke="#3f893b"></path><polyline id="Shape" fill="#3f893b" fill-rule="nonzero" points="4.86293961 4.09179688 2.5 0 0.13706039 4.09179688"></polyline></g></g></g></svg></span>
+          <span className="TextGreen">{` +${stockTicker.percentageChange}%`} <svg aria-hidden="true" width="5px" height="9px" viewBox="0 0 5 9" class=""><title>Group</title><defs></defs><g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd"><g id="Artboard"><g id="Group"><path d="M2.5,9 L2.5,3.373" id="Shape" stroke="#3f893b"></path><polyline id="Shape" fill="#3f893b" fillRule="nonzero" points="4.86293961 4.09179688 2.5 0 0.13706039 4.09179688"></polyline></g></g></g></svg></span>
         )}
       </p>
     </div>
